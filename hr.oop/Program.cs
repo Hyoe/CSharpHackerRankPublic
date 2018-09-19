788a9f26a3703f9948cbc9139cf00d0e0b4f5f69
Brian Boland <Brian.Boland@FiveSixTwo.com>
2018-09-18 23:41:32 -0700

    updates OOP instructions

--- hr.oop/Program.cs
+++ hr.oop/Program.cs
@@ -14,26 +14,26 @@ using System.Text;
 ///           -Name : string
 ///           -Age : int
 ///           -SortOrder : int
-///           -bread : string
+///           -Bread : string
 ///        -with the following methods
 ///           -Print()
 ///              -Displays 3 lines using Console.WriteLine()
 ///                 -Id - (dog's Id here)
 ///                 -Name - (dog's Name here)
-///                 -bread - (dog's bread here)
+///                 -Bread - (dog's bread here)
 ///     -Create a Cat class 
 ///        -with the following properties
 ///           -Id : int
 ///           -Name : string
 ///           -Age : int
 ///           -SortOrder : int
-///           -lives : int
+///           -Lives : int
 ///        -with the following methods
 ///           -Print()
 ///              -Displays 3 lines using Console.WriteLine()
 ///                 -Id - (cat's Id here)
 ///                 -Name - (cat's Name here)
-///                 -live - (cat's lives here)
+///                 -Lives - (cat's lives here)
 /// </summary>
 class Solution
 {
@@ -69,23 +69,23 @@ class Solution
 
         dogs = new List<Dog>()
         {
-            new Dog() {Id = 1, Name = "Fido", Age = 10, SortOrder = 2, bread = "Akita"},
-            new Dog() {Id = 2, Name = "Spot", Age = 1, SortOrder = 0, bread = "German Shepherd"},
-            new Dog() {Id = 3, Name = "Sam", Age = 4, SortOrder = 0, bread = "Labrador Retriever"},
-            new Dog() {Id = 4, Name = "Jim", Age = 12, SortOrder = 5, bread = "Akita"},
-            new Dog() {Id = 5, Name = "Spotty", Age = 2, SortOrder = 4, bread = "German Shepherd"},
-            new Dog() {Id = 6, Name = "Alfred", Age = 4, SortOrder = 1, bread = "Labrador Retriever"}
+            new Dog() {Id = 1, Name = "Fido", Age = 10, SortOrder = 2, Bread = "Akita"},
+            new Dog() {Id = 2, Name = "Spot", Age = 1, SortOrder = 0, Bread = "German Shepherd"},
+            new Dog() {Id = 3, Name = "Sam", Age = 4, SortOrder = 0, Bread = "Labrador Retriever"},
+            new Dog() {Id = 4, Name = "Jim", Age = 12, SortOrder = 5, Bread = "Akita"},
+            new Dog() {Id = 5, Name = "Spotty", Age = 2, SortOrder = 4, Bread = "German Shepherd"},
+            new Dog() {Id = 6, Name = "Alfred", Age = 4, SortOrder = 1, Bread = "Labrador Retriever"}
         };
 
 
         cats = new List<Cat>()
         {
-            new Cat() {Id = 1, Name = "Fido", Age = 10, SortOrder = 2, lives = 4},
-            new Cat() {Id = 2, Name = "Spot", Age = 1, SortOrder = 0, lives = 9},
-            new Cat() {Id = 3, Name = "Sam", Age = 4, SortOrder = 0, lives = 1},
-            new Cat() {Id = 4, Name = "Jim", Age = 12, SortOrder = 5, lives = 2},
-            new Cat() {Id = 5, Name = "Spotty", Age = 2, SortOrder = 4, lives = 5},
-            new Cat() {Id = 6, Name = "Alfred", Age = 4, SortOrder = 1, lives = 6}
+            new Cat() {Id = 1, Name = "Fido", Age = 10, SortOrder = 2, Lives = 4},
+            new Cat() {Id = 2, Name = "Spot", Age = 1, SortOrder = 0, Lives = 9},
+            new Cat() {Id = 3, Name = "Sam", Age = 4, SortOrder = 0, Lives = 1},
+            new Cat() {Id = 4, Name = "Jim", Age = 12, SortOrder = 5, Lives = 2},
+            new Cat() {Id = 5, Name = "Spotty", Age = 2, SortOrder = 4, Lives = 5},
+            new Cat() {Id = 6, Name = "Alfred", Age = 4, SortOrder = 1, Lives = 6}
         };
 
         foreach (var dog in dogs)
